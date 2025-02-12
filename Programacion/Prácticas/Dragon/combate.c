10afdb921410d602c365c7652598ecc1274a60af
pblovs <pablovise04@gmail.com>
2025-02-12 13:02:20 +0100

    p8

---
+++ Programacion/Prácticas/Dragon/combate.c
@@ -0,0 +1,19 @@
+#include "dragones.h"
+#include "Personajes.h"
+
+void combate(Dragon dragones[], Personaje Personajes[], int cantPersonajes, int cantDragones, int PersonajeElegido){
+
+	if(cantPersonajes == 0){
+		printf("GAME OVER\n");
+		return 0;
+	}
+	else if(cantDragones == 0){
+		printf("HAS GANADO\n");
+		return 0;
+	}
+
+
+
+	
+	
+}
---
+++ Programacion/Prácticas/Dragon/dragones.h
@@ -0,0 +1,10 @@
+
+typedef struct {
+    char nombre[50];
+    int vida;
+    int ataque;
+} Dragon;
+
+void inicializarDragones(Dragon dragones[], int *cantidad);
+void mostrarDragones(Dragon dragones[], int cantidad);
+
