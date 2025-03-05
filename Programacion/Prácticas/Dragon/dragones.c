25b449eb0167ff6f174bb12d0f7bb08899fa5d17
pblovs <pablovise04@gmail.com>
2025-03-05 14:27:52 +0100

    hacer boton

--- Programacion/Prácticas/Dragon/dragones.c
+++ Programacion/Prácticas/Dragon/dragones.c
@@ -7,15 +7,15 @@ void inicializarDragones(Dragon dragones[], int *cantidad) {
 
     strcpy(dragones[0].nombre, "Dragón de fuego");
     dragones[0].vida = 250;
-    dragones[0].ataque = 20; // entre 0 y 20;
+    dragones[0].ataque = 40; // entre 0 y 40;
 
     strcpy(dragones[1].nombre, "Dragón de hielo");
     dragones[1].vida = 300;
-    dragones[1].ataque = 30; // entre 0 y 30;
+    dragones[1].ataque = 50; // entre 0 y 50;
 
     strcpy(dragones[2].nombre, "Dragón eléctrico");
     dragones[2].vida = 200;
-    dragones[2].ataque = 50; // entre 0 y 50;
+    dragones[2].ataque = 70; // entre 0 y 70;
 
     *cantidad = 3; 
 }
